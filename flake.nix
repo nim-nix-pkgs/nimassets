4ce9e3f5a33c56ab5dd8162f84468ca42a13ac6f
hugosenari <hugosenari@gmail.com>
2022-05-09 20:40:52 -0300

    chore: re index nimassets

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."nimassets-master".dir   = "master";
-  inputs."nimassets-master".owner = "nim-nix-pkgs";
-  inputs."nimassets-master".ref   = "master";
-  inputs."nimassets-master".repo  = "nimassets";
-  inputs."nimassets-master".type  = "github";
-  inputs."nimassets-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nimassets-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
